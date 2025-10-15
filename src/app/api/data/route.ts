546a05b8209cd669cdf8f9279f62f45d3fc888fd
Matthew Foxwell <foxwellm@gmail.com>
2025-10-15 14:11:06 -0700

    feat: fetch all pages

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,12 +1,56 @@
 import { NextResponse } from "next/server";
 
+const defaultRefetchTime = 5;
+
+const getPatients = async (page: number) => {
+  for (let attempt = 1; attempt <= 3; attempt++) {
+    const response = await fetch(
+      `https://assessment.ksensetech.com/api/patients?page=${page}&limit=10`,
+      {
+        headers: {
+          "x-api-key": "ak_594ab648d6ce66cd9c5611ae512eda1cf2a0b4aab5c5c173",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const result = await response.json();
+
+    if (page === 1 && !result.pagination.totalPages) {
+      console.log(`No pagination provided, trying again after ${defaultRefetchTime} seconds`);
+      await new Promise((resolve) => setTimeout(resolve, defaultRefetchTime * 1000));
+      continue;
+    }
+
+    if (result?.error === "Service temporarily unavailable") {
+      console.log(
+        `Service temporarily unavailable, trying again after ${defaultRefetchTime} seconds`
+      );
+      await new Promise((resolve) => setTimeout(resolve, defaultRefetchTime * 1000));
+      continue;
+    }
+    if (result?.error === "Rate limit exceeded") {
+      const waitTime = result.retry_after;
+      console.log(`Rate limit exceeded, trying again after ${waitTime} seconds`);
+      await new Promise((resolve) => setTimeout(resolve, waitTime * 1000));
+      continue;
+    }
+
+    return result;
+  }
+  throw new Error("Unable to resolve all patients");
+};
+
 export async function GET() {
-  const response = await fetch("https://assessment.ksensetech.com/api/patients", {
-    headers: {
-      "x-api-key": "ak_594ab648d6ce66cd9c5611ae512eda1cf2a0b4aab5c5c173",
-      "Content-Type": "application/json",
-    },
-  });
-  const result = await response.json();
-  return NextResponse.json(result);
+  try {
+    const firstPage = await getPatients(1);
+    const totalPages = firstPage.pagination.totalPages;
+    const remainingPages = Array.from({ length: totalPages - 1 }, (_, i) => i + 2);
+
+    const results = await Promise.all(remainingPages.map(getPatients));
+
+    const allRecords = [...firstPage.data, ...results.flatMap((result) => result.data)];
+    return NextResponse.json(allRecords);
+  } catch {
+    return NextResponse.json({ error: "Unable to resolve all patients" });
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,29 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [patients, setPatients] = useState([]);
+  const [hasError, setHasError] = useState(false);
+
   useEffect(() => {
     const getPatients = async () => {
       const response = await fetch("/api/data");
       const result = await response.json();
-      setPatients(result.data);
+      
+      if (result?.error) {
+        setHasError(true);
+      } else {
+        setPatients(result);
+      }
     };
     getPatients();
   }, []);
+
+  if (hasError) {
+    return (
+      <main className="p-6 space-y-6 min-h-screen">
+        <h1>{hasError.toString()}</h1>
+      </main>
+    );
+  }
   return (
     <main className="p-6 space-y-6 min-h-screen">
       {patients.map((patient) => (
