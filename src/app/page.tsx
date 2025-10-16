cdba9273723252b86bae1d8f814290bdae985a05
Matthew Foxwell <foxwellm@gmail.com>
2025-10-15 17:17:57 -0700

    feat: table display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,36 @@
 "use client";
+import { Patient, PatientRisk } from "@/types/patient";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [patients, setPatients] = useState([]);
+  const [patients, setPatients] = useState<Patient[]>([]);
+  const [processed, setProcessed] = useState<PatientRisk | null>(null);
   const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getPatients = async () => {
       const response = await fetch("/api/data");
       const result = await response.json();
-      
+
       if (result?.error) {
         setHasError(true);
       } else {
-        setPatients(result);
+        setProcessed(result.processedPatients);
+        setPatients(result.allPatients);
       }
+      setIsLoading(false);
     };
     getPatients();
   }, []);
+
+  if (isLoading) {
+    return (
+      <main className="p-6 space-y-6 min-h-screen">
+        <h1>Loading</h1>
+      </main>
+    );
+  }
 
   if (hasError) {
     return (
@@ -28,9 +41,40 @@ export default function Home() {
   }
   return (
     <main className="p-6 space-y-6 min-h-screen">
-      {patients.map((patient) => (
-        <p key={patient.patient_id}>{patient.name}</p>
-      ))}
+      <table className="min-w-full border border-gray-300">
+        <thead>
+          <tr>
+            <th className="border px-4 py-2 text-left">Patient ID</th>
+            <th className="border px-4 py-2 text-left">Name</th>
+            <th className="border px-4 py-2 text-left">Age</th>
+            <th className="border px-4 py-2 text-left">Temperature</th>
+            <th className="border px-4 py-2 text-left">Blood Pressure</th>
+            <th className="border px-4 py-2 text-left">Is HIGH RISK</th>
+            <th className="border px-4 py-2 text-left">Is FEVER</th>
+            <th className="border px-4 py-2 text-left">Is DATA QUALITY</th>
+          </tr>
+        </thead>
+        <tbody>
+          {patients.map(({ patient_id, name, age, temperature, blood_pressure }) => (
+            <tr key={patient_id}>
+              <td className="border px-4 py-2">{patient_id}</td>
+              <td className="border px-4 py-2">{name}</td>
+              <td className="border px-4 py-2">{age}</td>
+              <td className="border px-4 py-2">{temperature}</td>
+              <td className="border px-4 py-2">{blood_pressure}</td>
+              <td className="border px-4 py-2">
+                {processed?.high_risk_patients.includes(patient_id) ? "Yes" : "no"}
+              </td>
+              <td className="border px-4 py-2">
+                {processed?.fever_patients.includes(patient_id) ? "Yes" : "no"}
+              </td>
+              <td className="border px-4 py-2">
+                {processed?.data_quality_issues.includes(patient_id) ? "Yes" : "no"}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 }
